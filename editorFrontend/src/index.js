60dfe56266df59501014a90623ad15a63a6c3152
Meet Jamsutkar <jamsutkarmeetpradeep@gmail.com>
2024-09-11 10:54:26 +0530

    navbar hehe

--- editorFrontend/src/index.js
+++ editorFrontend/src/index.js
@@ -1,7 +1,6 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, screen } = require('electron');
 const path = require('node:path');
-const { spawn } = require('child_process'); // Import the 'spawn' function
-
+const { spawn } = require('child_process');
 
 function startDjangoServer() {
   const djangoBackend = spawn('E:\\sem7\\Major-proj\\waumeditor\\backend\\env\\Scripts\\python.exe', [
@@ -11,54 +10,45 @@ function startDjangoServer() {
   ]);
 
   djangoBackend.stdout.on('data', (data) => {
-    console.log(`stdout:\n${data}`);
+    console.log(`Django server stdout:\n${data}`);
   });
 
   djangoBackend.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
+    console.error(`Django server stderr: ${data}`);
   });
 
   djangoBackend.on('error', (error) => {
-    console.error(`error: ${error.message}`);
+    console.error(`Django server error: ${error.message}`);
   });
 
   djangoBackend.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
+    console.log(`Django server child process exited with code ${code}`);
   });
 }
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
 const createWindow = () => {
-  // Create the browser window.
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: Math.min(width, 800),
+    height: Math.min(height, 600),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
-      
     },
   });
 
-  // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-  // Open the DevTools.
   mainWindow.webContents.openDevTools();
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   startDjangoServer();
   createWindow();
 
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
@@ -66,14 +56,8 @@ app.whenReady().then(() => {
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
